4dfbcddf9374f74e91a1f170ffc64c10bc269a4d
Schullert Gabriel <schullergabriel13@gmail.com>
2023-02-25 00:59:40 +0200

    Add CRUD functions for DevTaskRepository

--- TeamLeadHelp/Data/Repositories/DevTaskRepository.cs
+++ TeamLeadHelp/Data/Repositories/DevTaskRepository.cs
@@ -1,6 +1,54 @@
-﻿namespace TeamLeadHelp.Data.Repositories
+﻿using TeamLeadHelp.Models;
+
+namespace TeamLeadHelp.Data.Repositories
 {
-    public class DevTaskRepository
+    public class DevTaskRepository : IDevTaskRepository
     {
+        private readonly DevTaskContext _context;
+
+        public DevTaskRepository(DevTaskContext context)
+        {
+            _context = context;
+        }
+        public async Task<DevTask> Add(DevTask devTask)
+        {
+            _context.Tasks.AddAsync(devTask);
+            await _context.SaveChangesAsync();
+            return devTask;
+        }
+
+        public async Task<DevTask> Delete(int id)
+        {
+            var devTask = await _context.Tasks.FindAsync(id);
+            if (devTask == null) return null;
+            _context.Tasks.Remove(devTask);
+            await _context.SaveChangesAsync();
+            return devTask;
+        }
+
+        public Task<IEnumerable<DevTask>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DevTask>> GetAllOnHold()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DevTask> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DevTask>> GetTasksInProgress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DevTask> Update(DevTask devTask)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- TeamLeadHelp/Data/Repositories/IDevTaskRepository.cs
+++ TeamLeadHelp/Data/Repositories/IDevTaskRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace TeamLeadHelp.Data.Repositories
+﻿using TeamLeadHelp.Models;
+
+namespace TeamLeadHelp.Data.Repositories
 {
     public interface IDevTaskRepository
     {
+        Task<IEnumerable<DevTask>> GetAll();
+        Task<IEnumerable<DevTask>> GetAllOnHold();
+        Task<IEnumerable<DevTask>> GetTasksInProgress();
+        Task<DevTask> GetById(int id);
+        Task<DevTask> Add(DevTask product);
+        Task<DevTask> Update(DevTask product);
+        Task<DevTask> Delete(int id);
     }
 }
